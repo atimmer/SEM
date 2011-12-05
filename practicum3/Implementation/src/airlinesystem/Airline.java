d135f03a89a9ca4f6f93e0f6971e52ede3cadb32
Anton Timmermans <timmermansanton@gmail.com>
2011-12-05 17:53:56 +0100

    Adds some javadoc

--- practicum3/Implementation/src/airlinesystem/Airline.java
+++ practicum3/Implementation/src/airlinesystem/Airline.java
@@ -24,30 +24,87 @@ public class Airline {
 	// All kinds of creation methods here :)
 	// This is where the magic happens
 	
+	/**
+	 * Creates a booking for a certain person on a flight
+	 * 
+	 * @param person The person that goes on a flight
+	 * @param flight The flight the person goes with
+	 * @require person != null
+	 * @require flight != null
+	 * @ensure result != null
+	 * @return The newly created booking
+	 */
 	public Booking createBooking(Person person, Flight flight) {
 		Booking b = new Booking(person, flight);
 		bookings.add(b);
 		return b;
 	}
 	
+	/**
+	 * Creates a new person, with an identifier and a name
+	 * 
+	 * @param identifier The identifier the person should get
+	 * @param name The name the person has
+	 * 
+	 * @require name != null
+	 * @ensure result != null
+	 * 
+	 * @return The newly created person
+	 */
 	public Person createPerson(int identifier, String name) {
 		Person p = new Person(identifier, name);
 		persons.add(p);
 		return p;
 	}
 	
+	/**
+	 * Creates a new employee, with an identifier, a name and a job description
+	 * 
+	 * @param identifier The identifier the employee should get
+	 * @param name The name the employee has
+	 * @param jobDescription The description of the job this employee is doing
+	 * 
+	 * @require name != null && jobDescription != null
+	 * @ensure result != null
+	 * 
+	 * @return The newly created employee
+	 */
 	public Employee createEmployee(int identifier, String name, String jobDescription) {
 		Employee e = new Employee(identifier, name, jobDescription);
 		persons.add(e);
 		return e;
 	}
 	
+	/**
+	 * Creates a new flight from a flight template
+	 * 
+	 * @param template the template used to create this flight
+	 * @param date The date on which this flight flies
+	 *
+	 * @require template != null && date != null
+	 * @ensure result != null
+	 * 
+	 * @return The newly created flight
+	 */
 	public Flight createFlightFromTemplate(FlightTemplate template, Date date) {
 		Flight f = new Flight(template, date);
 		flights.add(f);
 		return f;
 	}
 	
+	/**
+	 * Creates a new flight template
+	 * 
+	 * @param flightNumber The number this flight template should get
+	 * @param origin       The origin from which all flights created with this template will fly
+	 * @param destination  The destination to which all flights created with this template will fly
+	 * @param time         The time at which all flights created with this template will fly
+	 * 
+	 * @require origin != null && destination != null
+	 * @ensure result != null
+	 * 
+	 * @return The newly created flight template
+	 */
 	public FlightTemplate createFlightTemplate(int flightNumber, String origin, String destination, int time) {
 		FlightTemplate ft = new FlightTemplate(flightNumber, origin, destination, time);
 		flightTemplates.add(ft);
@@ -56,6 +113,13 @@ public class Airline {
 	
 	// All kinds of search / retrieve methods
 	
+	/**
+	 * Gets a person
+	 * 
+	 * @param identifier The identifier of the person the client wants to get
+	 * 
+	 * @return the retrieved person
+	 */
 	public Person personWithIdentifier(int identifier) {
 		Person result = null;
 		for(int i = 0; i < persons.size() && result == null; i++) {
@@ -66,6 +130,14 @@ public class Airline {
 		return result;
 	}
 	
+	/**
+	 * Gets a flight
+	 * 
+	 * @param flightNumber The number of the flight the client wants to get
+	 * @param date The date of the flight the client wants to get
+	 * 
+	 * @return The retrieved flight
+	 */
 	public Flight flightWithNumberAndDate(int flightNumber, Date date) {
 		Flight result = null;
 		for(int i = 0; i < flights.size() && result == null; i++) {
@@ -76,6 +148,13 @@ public class Airline {
 		return result;
 	}
 	
+	/**
+	 * Gets a flight template
+	 * 
+	 * @param flightNumber the number of the flight template the client wants to get
+	 * 
+	 * @return The retrieved flight template
+	 */
 	public FlightTemplate flightTemplateWithNumber(int flightNumber) {
 		FlightTemplate result = null;
 		for(int i = 0; i < flightTemplates.size() && result == null; i++) {
@@ -86,6 +165,15 @@ public class Airline {
 		return result;
 	}
 	
+	/**
+	 * Gets a list of flights with the origin, destination and date specified
+	 * 
+	 * @param origin The origin of the flights the client wants to get
+	 * @param destination The destination of flights the client wants to get
+	 * @param date The date of flights the client wants to get
+	 * 
+	 * @return The retrieved flights
+	 */
 	public ArrayList<Flight> flightsFromToWithDate(String origin, String destination, Date date) {
 		ArrayList<Flight> result = new ArrayList<Flight>();
 		for(int i = 0; i < flights.size(); i++) {
@@ -96,15 +184,14 @@ public class Airline {
 		return result;
 	}
 	
-	public String toString() {
-		String result; 
-		result  = String.format("%-15s %s\n", "Persons", persons);
-		result += String.format("%-15s %s\n", "Bookings", bookings);
-		result += String.format("%-15s %s\n", "Flights", flights);
-		result += String.format("%-15s %s\n", "Templates", flightTemplates);
-		return result;
-	}
-
+	/**
+	 * Indicates whether or not the person has a booking on the flight
+	 * 
+	 * @param person The person that might have a booking on the specified flight
+	 * @param flight The flight the person might have a booking on
+	 * 
+	 * @return Whether or not the person has a booking on the flight
+	 */
 	public boolean personHasBookingForFlight(Person person, Flight flight) {
 		boolean result = false;
 		for(int i = 0; i < bookings.size() && !result; i++) {
@@ -115,4 +202,13 @@ public class Airline {
 		return result;
 	}
 	
+	@Override
+	public String toString() {
+		String result; 
+		result  = String.format("%-15s %s\n", "Persons", persons);
+		result += String.format("%-15s %s\n", "Bookings", bookings);
+		result += String.format("%-15s %s\n", "Flights", flights);
+		result += String.format("%-15s %s\n", "Templates", flightTemplates);
+		return result;
+	}	
 }
