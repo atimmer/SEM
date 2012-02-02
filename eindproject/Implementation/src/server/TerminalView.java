3fecee014a470ea4efb5fbdb46180343e0f3e745
Anton Timmermans <timmermansanton@gmail.com>
2012-02-02 02:14:24 +0100

    Fixes my commit status

---
+++ eindproject/Implementation/src/server/TerminalView.java
@@ -0,0 +1,12 @@
+package server;
+
+public class TerminalView {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Terminal View of the server has started");
+	}
+
+}
